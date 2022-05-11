ec7ea3d1a34ea4ad9e2e8f99e461140848a66d65
Arman <persakiss@gmail.com>
2022-05-11 20:23:02 +0400

    Adding pug

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const { application } = require('express')
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const port = 3000
 
-app.listen(3000)
+app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-res.send('Hello World!')
+    console.log('Here')
+    res.render('index')
 })
-app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`)
-})
+
+app.listen(port)
+
+
+
+
+
+

353c6f962431f8b488a1ced575d3ab21b450af1e
Arman <persakiss@gmail.com>
2022-05-11 19:36:19 +0400

    listen port 3000

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const { application } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.listen(3000)
+
 app.get('/', (req, res) => {
 res.send('Hello World!')
 })

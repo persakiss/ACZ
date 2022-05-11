faa60d9f1914ffce08c2018977449852159fe8fd
Arman <persakiss@gmail.com>
2022-05-11 21:50:26 +0400

    Middleware

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const router = express.Router()
 
+// Routes are read from top to bottom,
+// so make sure you're declaring in order
+// Put static routes BEFORE dynamic routes
 
+
+
+router.use(userLogger)
 
 router.get('/', (req, res) => {
     res.send("User List")
@@ -19,8 +25,52 @@ router.post('/', (req, res) => {
 
 // Dynamic parameter starts with :
 // For getting an individual user 
+
+router.route("/:id")
+.get((req, res) => {
+    // console.log(req.user)
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Put User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+
+/*
+// The code above replaces the more
+// verbose code below. The only difference
+// is we only had to define our route in one
+// location, and all the requests match the route
+
 router.get('/:id', (req, res) => {
     res.send(`Get User With ID ${req.params.id}`)
 })
+
+router.put('/:id', (req, res) => {
+    res.send(`Put User With ID ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+}) 
+*/
+
+const users = [{ name: "Dino" }, { name: "Sabrina" }, { name: "Arman"}]
+
+// param is a type of middleware, stuff that runs
+// betwene the request and the response.
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
+// Every piece of middleware takes a req, res, next
+function userLogger(req, res, next) {
+    console.log("User: " + req.originalUrl)
+    next()
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const { application } = require('express')
 const express = require('express')
-const res = require('express/lib/response')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-    console.log('Here')
+// Render is done top-to-bottom, so placement is key.
+// If you have middleware (eg logger) to use everywhere,
+// Define it at the top.
+// app.use(logger) 
+
+
+// If you have middleware you only want to run at individual
+// routes/endpoints, you can indclude it is an argument
+app.get('/', logger, (req, res) => {
+    // console.log('Here')
     res.render('index', { text: 'Thanks for stopping by!'})
 })
 
@@ -15,6 +21,13 @@ app.get('/', (req, res) => {
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
+
+
+// Every piece of middleware takes a req, res, next
+function logger(req, res, next) {
+    console.log("Log: " + req.originalUrl)
+    next()
+}
 
 app.listen(port)
 

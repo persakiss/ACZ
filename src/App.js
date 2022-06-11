fd33bb108410f0bb2a7907b73e4e91058565a06b
Arman <persakiss@gmail.com>
2022-06-11 16:46:54 +0400

    event handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Contact from './components/Contact';
 import { Greeting } from './components/Greeting';
+import { Button } from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <Greeting name="Guest" />
       <Contact />
+      <Button />
     </div>
   );
 }

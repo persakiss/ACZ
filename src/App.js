051621409c4fed370c5fec407bf7ca45d83db547
Arman <persakiss@gmail.com>
2022-06-11 14:47:03 +0400

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Contact from './components/Contact';
+import { Greeting } from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
+      <Greeting name="Guest" />
       <Contact />
     </div>
   );

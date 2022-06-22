8026072154002b0def669dad6da55c6f2a651d5c
Arman <persakiss@gmail.com>
2022-06-22 17:19:58 +0400

    removing unnecessary lines

--- src/components/BottomLine/BottomLine.js
+++ src/components/BottomLine/BottomLine.js
@@ -1,16 +1,14 @@
-import React, { useRef } from "react";
+import React from "react";
 import LeftText from "../LeftText/LeftText";
 import TextInput from "../TextInput/TextInput";
 import './BottomLine.css';
 
 function BottomLine() {
-  const inputRef = useRef();
-  setTimeout(() => inputRef.current.focus(), 1000);
 
   return (
     <label id="bottom" for="console">
       <LeftText />
-      <TextInput ref={inputRef} onLayout={setTimeout} />
+      <TextInput />
     </label>
   );
 }

6bdae7fbe1432cc069f002ba8112bf48115575fc
Arman <persakiss@gmail.com>
2022-06-22 12:39:20 +0400

    revamp start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Contact from './components/Contact';
-import { Greeting } from './components/Greeting';
-import { Talker } from './components/Talker';
+
 
 function App() {
   return (
-    <div className="App">
-      <Greeting name="Guest" />
-      <Contact />
-      <Talker />
+    <div>
     </div>
   );
 }
